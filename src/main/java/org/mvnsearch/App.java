1574f5672b31a6aaa0bbb6e20b4f74f595e0cbd2
Damir Isanbirdin <damir.isanbirdin@gmail.com>
2019-05-20 01:28:51 +0200

    fixed expose static resources. Now static resources like css, js can be used via auto configuration

--- src/main/java/org/mvnsearch/App.java
+++ src/main/java/org/mvnsearch/App.java
@@ -2,13 +2,22 @@ package org.mvnsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * spring boot application
  */
 @SpringBootApplication
-public class App {
+public class App implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new RockerMessageConverter());
     }
 }
